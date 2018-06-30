b9001415cb5f8d8ba081e26e1c0acfab940f80d6
Nicole Cooper-White <nwhite1018@gmail.com>
2018-06-30 11:21:52 -0600

    firebase

--- logic.js
+++ logic.js
@@ -1,17 +1,32 @@
 $("#addButton").on("click", function(event){
     event.preventDefault();
     console.log("clicked");
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//Initial Values
+var name = "";
+var role = "";
+var startDate = "";
+var rate = 0;
 
     //grab inputs
-var name = $("#name-input").val().trim();
-var role = $("#role-input").val().trim();
-var startDate = $("#start-date").val().trim();
-var rate = $("#monthly-rate").val().trim();
+name = $("#name-input").val().trim();
+role = $("#role-input").val().trim();
+startDate = $("#start-date").val().trim();
+rate = $("#monthly-rate").val().trim();
 
 console.log(name);
 console.log(role);
 console.log(startDate);
 console.log(rate);
+// Code for "setting values in the database"
+database.ref().push({
+    name: name,
+    role = role,
+    startDate = startDate,
+    rate = rate
+});
 
 //update the rows on the html
     var addEmployee = $("<tr>");
