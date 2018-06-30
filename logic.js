4e079b29c6069b08565897dea034e71e65a79156
Nicole Cooper-White <nwhite1018@gmail.com>
2018-06-30 11:25:24 -0600

    intializing the database

--- logic.js
+++ logic.js
@@ -1,49 +1,51 @@
-$("#addButton").on("click", function(event){
+ 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyD1F6HkOSNomp5qXzTZq3rJmD3GmNjSv_c",
+        authDomain: "group-project-c5951.firebaseapp.com",
+        databaseURL: "https://group-project-c5951.firebaseio.com",
+        projectId: "group-project-c5951",
+        storageBucket: "group-project-c5951.appspot.com",
+        messagingSenderId: "205795254538"
+    };
+    firebase.initializeApp(config);
+    
+    $("#addButton").on("click", function (event) {
     event.preventDefault();
     console.log("clicked");
-firebase.initializeApp(config);
-var database = firebase.database();
+    
+    var database = firebase.database();
 
-//Initial Values
-var name = "";
-var role = "";
-var startDate = "";
-var rate = 0;
+    //Initial Values
+    var name = "";
+    var role = "";
+    var startDate = "";
+    var rate = 0;
 
     //grab inputs
-name = $("#name-input").val().trim();
-role = $("#role-input").val().trim();
-startDate = $("#start-date").val().trim();
-rate = $("#monthly-rate").val().trim();
+    name = $("#name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#start-date").val().trim();
+    rate = $("#monthly-rate").val().trim();
 
-console.log(name);
-console.log(role);
-console.log(startDate);
-console.log(rate);
-// Code for "setting values in the database"
-database.ref().push({
-    name: name,
-    role = role,
-    startDate = startDate,
-    rate = rate
-});
+    console.log(name);
+    console.log(role);
+    console.log(startDate);
+    console.log(rate);
+    // Code for "setting values in the database"
+    database.ref().push({
+        name: name,
+        role = role,
+        startDate = startDate,
+        rate = rate
+    });
 
-//update the rows on the html
+    //update the rows on the html
     var addEmployee = $("<tr>");
 
-$("#employeeList").append(addEmployee);
+    $("#employeeList").append(addEmployee);
 
-<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyD1F6HkOSNomp5qXzTZq3rJmD3GmNjSv_c",
-   authDomain: "group-project-c5951.firebaseapp.com",
-   databaseURL: "https://group-project-c5951.firebaseio.com",
-   projectId: "group-project-c5951",
-   storageBucket: "group-project-c5951.appspot.com",
-   messagingSenderId: "205795254538"
- };
- firebase.initializeApp(config);
+   
 
 
 })
